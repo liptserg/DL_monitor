662b48bac306002c0d60a51e47ea103c247fdbd1
tserg <liptserg@yandex.ru>
2021-06-29 17:53:01 +0300

    describe windows

---
+++ gui.py
@@ -0,0 +1,26 @@
+import tkinter as tk
+
+class Gui():
+    def __init__(self):
+        self.root = tk.Tk()
+        self.settingWindow()
+        self.declareWidget()
+
+    def settingWindow(self):
+        self.root.title('Монитор DL')
+        self.root.geometry('600x400')
+
+    def declareWidget(self):
+        self.strLbl01_begin = 'Время запуска предыдущей проверки: '
+        self.strLbl01 = self.strLbl01_begin + '00:00'
+        self.strLbl02 = 'Интервал 20 мин '
+        self.lbl01 = tk.Label(text=self.strLbl01)
+        self.lbl02 = tk.Label(text=strLbl02)
+        self.btnExit = tk.Button(text='Выход', command=exit)
+        self.txtMain = Text()
+
+    def placeWidget(self):
+        self.lbl01.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
+        self.lbl02.grid(row=0, column=4, padx=10, pady=10)
+        self.btnExit.grid(row=0, column=8, padx=10, pady=10)
+        self.txtMain.grid(row=1, column=0, columnspan=10)
